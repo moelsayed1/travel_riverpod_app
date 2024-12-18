ee65b537360bb8236f848a9a6b6e2d9caa260f7a
moelsayed1 <mostafaembaby19@gmail.com>
2024-12-18 07:04:57 +0200

    create local datasource

---
+++ lib/Features/trip/data/data_sources/trip_local_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:traver_riverpod_app/Features/trip/data/models/trip_model.dart';
+
+class TripLocalDataSource {
+
+  final Box<TripModel> tripBox;
+
+  TripLocalDataSource(this.tripBox);
+
+  List<TripModel> getTrips() {
+    return tripBox.values.toList();
+  }
+
+  void addTrip(TripModel trip) {
+    tripBox.add(trip);
+  }
+
+  void deleteTrip(int index) {
+    tripBox.deleteAt(index);
+  }
+}
